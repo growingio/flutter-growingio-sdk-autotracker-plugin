a93d8c6af4ef013f6511baac95169fa74815af62
sheng <277715243@qq.com>
2021-10-15 12:26:02 +0800

    feat: webcircle支持

--- example/android/app/src/main/java/com/growingio/growingio_sdk_autotracker_plugin_example/MyApplication.java
+++ example/android/app/src/main/java/com/growingio/growingio_sdk_autotracker_plugin_example/MyApplication.java
@@ -1,21 +1,19 @@
 package com.growingio.growingio_sdk_autotracker_plugin_example;
 
-import com.growingio.android.sdk.autotrack.AutotrackConfiguration;
+import com.growingio.android.sdk.autotrack.CdpAutotrackConfiguration;
 import com.growingio.android.sdk.autotrack.GrowingAutotracker;
 import io.flutter.app.FlutterApplication;
 
 public class MyApplication extends FlutterApplication {
-    private static AutotrackConfiguration sConfiguration;
+    private static CdpAutotrackConfiguration sConfiguration;
     @Override
     public void onCreate() {
         super.onCreate();
-
-
         if (sConfiguration == null) {
-            sConfiguration = new AutotrackConfiguration("bfc5d6a3693a110d", "growing.d80871b41ef40518")
+            sConfiguration = new CdpAutotrackConfiguration("bfc5d6a3693a110d", "growing.d80871b41ef40518")
                     .setUploadExceptionEnabled(false)
-                    .setDebugEnabled(true)
-                    .setOaidEnabled(false);
+                    .setDataSourceId("8deb3a4737d7aa00")
+                    .setDebugEnabled(true);
         }
         GrowingAutotracker.startWithConfiguration(this, sConfiguration);
     }
--- ios/Classes/GrowingioSdkAutotrackerPlugin.m
+++ ios/Classes/GrowingioSdkAutotrackerPlugin.m
@@ -1,8 +1,9 @@
 #import "GrowingioSdkAutotrackerPlugin.h"
-#import <GrowingAnalytics/GrowingAutotracker.h>
+#import <GrowingAnalytics-cdp/GrowingAutotracker.h>
 #import <GrowingAnalytics/GrowingEventManager.h>
 #import <GrowingAnalytics/GrowingViewElementEvent.h>
 #import <GrowingAnalytics/GrowingPageEvent.h>
+#import <GrowingAnalytics/GrowingFlutterPlugin.h>
 
 @implementation GrowingioSdkAutotrackerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -10,6 +11,14 @@
       methodChannelWithName:@"growingio_sdk_autotracker_plugin"
             binaryMessenger:[registrar messenger]];
   GrowingioSdkAutotrackerPlugin* instance = [[GrowingioSdkAutotrackerPlugin alloc] init];
+    [GrowingFlutterPlugin sharedInstance].onNativeCircleStart = ^{
+        [channel invokeMethod:@"WebCircle" arguments:@YES];
+    };
+    [GrowingFlutterPlugin sharedInstance].onNativeCircleStop = ^{
+        [channel invokeMethod:@"WebCircle" arguments:@NO];
+    };
+    
+    
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
@@ -32,10 +41,6 @@
     }else if ([methodName isEqualToString:@"setLoginUserAttributes"]) {
         NSLog(@"Handler setLoginUserAttributes");
         [[GrowingAutotracker sharedInstance] setLoginUserAttributes:argDic];
-    }else if ([methodName isEqualToString:@"setVisitorAttributes"]) {
-        [[GrowingAutotracker sharedInstance] setVisitorAttributes:argDic];
-    }else if ([methodName isEqualToString:@"setConversionVariables"]) {
-        [[GrowingAutotracker sharedInstance] setVisitorAttributes:argDic];
     }else if ([methodName isEqualToString:@"setLoginUserId"]) {
         NSString *userId = argDic[@"userId"];
         [[GrowingAutotracker sharedInstance] setLoginUserId:userId];
@@ -49,7 +54,15 @@
         long long ts = ((NSNumber *)argDic[@"timestamp"]).longLongValue;
         GrowingBaseBuilder *builder =
             GrowingPageEvent.builder.setTitle(argDic[@"title"]).setPath(argDic[@"path"]).setTimestamp(ts);
-        [[GrowingEventManager shareInstance] postEventBuidler:builder];
+        [[GrowingEventManager sharedInstance] postEventBuidler:builder];
+    }else if ([methodName isEqualToString:@"flutterWebCircleEvent"]) {
+        NSLog(@"flutterWebCircleEvent %@",argDic);
+        if ([GrowingFlutterPlugin sharedInstance].onFlutterCircleData) {
+            [GrowingFlutterPlugin sharedInstance].onFlutterCircleData(argDic);
+        }else {
+            NSLog(@"flutterWebCircleEvent未检测到开始圈选，数据无法传输");
+        }
+
     }else {
         return NO;
     }
@@ -65,7 +78,7 @@
          .setXpath(argDic[@"xpath"])
          .setIndex(index)
         .setTextValue(argDic[@"textValue"]);
-        [[GrowingEventManager shareInstance] postEventBuidler:builder];
+        [[GrowingEventManager sharedInstance] postEventBuidler:builder];
 }
 
 @end
